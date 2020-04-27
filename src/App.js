19eaa058ec2d2624dc9b4848c99f38f5a905a6ce
William Sterling <sterlinw@b19945.local>
2020-04-27 00:31:33 -0600

    progress bar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from 'react';
-import './App.css';import { createMuiTheme, makeStyles, ThemeProvider } from '@material-ui/core/styles';
+import './App.css';
+import { createMuiTheme, makeStyles, ThemeProvider } from '@material-ui/core/styles';
 import Header from './components/Header';
 import StartApp from './components/StartApp';
-
+import ProgressBar from './components/ProgressBar';
+import Offer from './offer/Offer';
+import Need from './need/Need';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 const palette = createMuiTheme({
   palette: {
@@ -36,18 +45,66 @@ const useStyles = makeStyles(theme => ({
   },
 }));
 
+
 export default function ButtonAppBar() {
   const classes = useStyles();
 
   
   return (
+
     <div className={classes.root}>
       <ThemeProvider theme={palette}>
-
+      <Router>
+        {/* Progress Bar */}
+        <Switch>
+            <Route exact path="/">
+              <ProgressBar
+                progressAmount= '10'
+              />
+            </Route>
+            <Route exact path="/home">
+              <ProgressBar
+                progressAmount= '10'
+              />
+            </Route>
+            <Route path="/offer">
+              <ProgressBar
+                progressAmount= '20'
+              />
+            </Route>
+            <Route path="/need">
+              <ProgressBar
+                progressAmount= '20'
+              />
+            </Route>
+          </Switch>
+        </Router>
+        
         <Header/>
 
-        <StartApp/>
-
+        <Router>
+          {/*
+            A <Switch> looks through all its children <Route>
+            elements and renders the first one whose path
+            matches the current URL. Use a <Switch> any time
+            you have multiple routes, but you want only one
+            of them to render at a time
+          */}
+          <Switch>
+            <Route exact path="/">
+              <StartApp/>
+            </Route>
+            <Route path="/home">
+              <StartApp/>
+            </Route>
+            <Route path="/offer">
+              <Offer />
+            </Route>
+            <Route path="/need">
+              <Need />
+            </Route>
+          </Switch>
+        </Router>
 
       </ThemeProvider>
     </div>
