1f4bd1af5add08f64283a24233fed1cc4d301212
Shadow <bill@indigotheory.io>
2020-05-25 22:20:37 -0700

    styling voteDetails

---
+++ src/components/sidebar/RValue.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './attributeScale.css';
+
+function LocationScore(){
+    return(
+        <div>
+            <div className="score-attribute">
+                <div className="attribute-title">
+                    <label>R-Value</label>
+                    <span>92%</span>
+                </div>
+                <div className="attribute-scale">
+                    <div className="attribute-progress rValue"></div>
+                </div>
+            </div>
+        </div>
+    )
+    
+}
+export default LocationScore
