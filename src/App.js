d20a8bc3e1b4673db1e30103c512c6d2da373755
stoner <stonroner@gmail.com>
2020-05-30 10:52:37 -0600

    Fixed routing after bad merge

--- src/App.js
+++ src/App.js
@@ -5,17 +5,16 @@ import Home from './home/home'
 import Need from './need/Need'
 import Supply from './supply/Supply';
 import VoteNeed from './VoteNeed/VoteNeed'
-
 import VoteDetail from "./VoteNeed/VoteDetail"
 import { Button, Main} from '@aragon/ui'
 import { ThemeProvider } from '@chakra-ui/core'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import './App.css'
 
 
 function App() {
   return (
     <Main>
+      
       <Router>
           <Switch>
               <Route path="/voteneed">
@@ -35,12 +34,7 @@ function App() {
               </Route>
           </Switch>
       </Router>
-      
-      <SidePanel
-        title="Add tokens"
-        opened={sidePanelOpened}
-        onClose={() => setSidePanelOpened(false)}
-      />
+
     </Main>
   )
 }
