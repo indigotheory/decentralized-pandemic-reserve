20d8045bf97fb51784feae4c566d75846d2de026
Shadow <bill@indigotheory.io>
2020-05-30 08:31:07 -0700

    fixing routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react'
 import ReactDOM from "react-dom"
-import {Switch, Route, BrowserRouter} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Home from './home/home'
+import Need from './need/Need'
+import Supply from './supply/Supply';
 import VoteNeed from './VoteNeed/VoteNeed'
 import VoteDetail from "./VoteNeed/VoteDetail"
+import { Button, Main} from '@aragon/ui'
 import { ThemeProvider } from '@chakra-ui/core'
 import './App.css'
 
@@ -24,18 +27,29 @@ import './App.css'
 
 function App() {
   return (
-    <ThemeProvider>
-      <Home />
-    </ThemeProvider>
+    <Main>
+      <Router>
+          <Switch>
+              <Route path="/voteneed">
+                <VoteNeed />
+              </Route>
+              <Route path="/need">
+                <Need />
+              </Route>
+              <Route path="/supply">
+                <Supply />
+              </Route>
+              <Route path="/votedetail">
+                <VoteDetail />
+              </Route>
+              <Route path="/">
+                <Home />
+              </Route>
+          </Switch>
+      </Router>
+      
+    </Main>
   )
 }
-
-//Render app into the root HTML DOM node
-ReactDOM.render(
-  <BrowserRouter>
-      <App />
-  </BrowserRouter>,
-  document.getElementById('root')
-)
 
 export default App;
--- src/home/home.js
+++ src/home/home.js
@@ -7,64 +7,70 @@ import Header from '../components/Header.js'
 
 //Chakra UI - Added by stoner. Mobile compliant if we care about that
 //https://chakra-ui.com/getting-started
+// We're trying to leverage AragonUI. You won't need another Library.
 import {
     Box,
     Flex,
     Image,
     Button,
     ButtonGroup,
-    Link,
     PseudoBox,
     Stack,
     Text,
     ThemeProvider 
 } from '@chakra-ui/core'
+import { Link, Router } from 'react-router-dom';
 
 function Home() {
     return (
     <ThemeProvider>
-      <Main>
-            <Box pt={[2]} m={[4]}></Box>
+        <Main>
+            <Box pt={[2]} m={[4]}>
                 <Stack spacing={4} align="center">
-                    <Flex align="center">
-                        <Image
-                            //TODO: Figure out why src isn't working, but fallback does 
-                            src={'../img/logo.png'}
-                            size={384}
-                            fallbackSrc='https://github.com/indigotheory/DPR-MarketPlace/raw/master/img/icon_sq.png'
-                            alt=''
-                        />
-                    </Flex>
-                    
-                    {/* TODO: Theme this text with Oswald font */}
-                    
-                    <Flex>
-                        <Box mt="2">
-                            <Text fontSize="32px">Welcome to</Text>
-                            <Text fontSize="40px">the Decentralized Pandemic Reserve</Text>
-                        </Box>
-                    </Flex>
-                    <ButtonGroup p="4">
-                        <Button 
-                            mr="6" 
-                            variantColor="purple" 
-                            size="lg">
-                            I have a need
-                        </Button>
-                        <Button 
-                            ml="6" 
-                            variantColor="purple" 
-                            size="lg">
-                            I can supply
-                        </Button>
-                        <Button 
-                            ml="6" 
-                            variantColor="purple" 
-                            size="lg">
-                            See votes
-                        </Button>
-                    </ButtonGroup>
-                </Stack>
+            <Flex align="center">
+                <Image
+                    //TODO: Figure out why src isn't working, but fallback does 
+                    src={'../img/logo.png'}
+                    size={384}
+                    fallbackSrc='https://github.com/indigotheory/DPR-MarketPlace/raw/master/img/icon_sq.png'
+                    alt=''
+                />
+            </Flex>
+            
+            {/* TODO: Theme this text with Oswald font */}
+            
+            <Flex>
+                <Box mt="2">
+                    <Text fontSize="32px">Welcome to</Text>
+                    <Text fontSize="40px">the Decentralized Pandemic Reserve</Text>
+                </Box>
+            </Flex>
+            <ButtonGroup p="4">
+                <Link to="/need">
+                    <Button 
+                    mr="6" 
+                    variantColor="purple" 
+                    size="lg">
+                    I have a need
+                    </Button>
+                </Link>
+                <Link to="/supply">
+                    <Button 
+                        ml="6" 
+                        variantColor="purple" 
+                        size="lg">
+                        I can supply
+                    </Button>
+                </Link>
+                <Link to="/voteneed"><Button 
+                    ml="6" 
+                    variantColor="purple" 
+                    size="lg">
+                    See votes
+                </Button></Link>
+            </ButtonGroup>
+        </Stack>
+            </Box>
       </Main>
       </ThemeProvider>
     )
