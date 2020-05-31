474950b139e2b00549df6e72ef392a8a6669d96d
stoner <stonroner@gmail.com>
2020-05-31 00:50:07 -0600

    Revised need to use icons similar to supply

--- src/need/NeedStart.js
+++ src/need/NeedStart.js
@@ -1,29 +1,59 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom'
 import ProgressBar from '../components/ProgressBar'
-import { Button, DropDown, TextInput } from '@aragon/ui';
+import { Button, DropDown, Card, EmptyStateCard, TextInput } from '@aragon/ui';
 
 function NeedStart(){
   const [value, setValue] = useState('')
   const [selected, setSelected] = useState(0)
 
     return(
-        <div>
+        <div className="container">
+
+        <div className="contentContainer form centerForm">
             <ProgressBar/>
-            <div className="contentContainer form_sm form">
-                <label>What do you need?</label>
-                <DropDown
-                items={['Raw Materials / Components', 'Labor / Volunteers', 'Production Process / Equipment','Production Space (rental / share)','Finished Product']}
-                selected={selected}
-                onChange={setSelected}
-                className="dropDown"
-                />
+            <h4>I need</h4>
+            <div className="cardLayout">
+                <Card className="cardOption">
+                    <Link to="/"><img src={require('../img/raw-materials.svg')} alt="Raw Materials Icon" className="cardIcon"/></Link>
+                    <label>Raw Materials / Components</label>
+                    <Link to="/need/material">
+                        <Button mode="strong" label="Next" className="actionButton"/>
+                    </Link>
+                </Card>
 
-                {/* TODO: Switch on this to go to seperate need pages */}
+                <Card className="cardOption">
+                    <Link to="/"><img src={require('../img/labor.svg')} alt="Raw Materials Icon" className="cardIcon"/></Link>
+                    <label>Labor / Volunteers</label>
+                    <Link to="/need/labor">
+                        <Button mode="strong" label="Next" className="actionButton"/>
+                    </Link>
+                </Card>
 
-                <Link to="/need/material">
-                <Button mode="strong" label="Next" className="actionButton"/>
-                </Link>
+                <Card className="cardOption">
+                    <Link to="/"><img src={require('../img/production.svg')} alt="Raw Materials Icon" className="cardIcon"/></Link>
+                    <label>Production Process / Equipment</label>
+                    <Link to="/need/process">
+                        <Button mode="strong" label="Next" className="actionButton"/>
+                    </Link>
+                </Card>
+
+                <Card className="cardOption">
+                    <Link to="/"><img src={require('../img/space.svg')} alt="Raw Materials Icon" className="cardIcon"/></Link>
+                    <label>Production Space (rental / share)</label>
+                    <Link to="/need/space">
+                        <Button mode="strong" label="Next" className="actionButton"/>
+                    </Link>
+                </Card>
+
+                <Card className="cardOption">
+                    <Link to="/"><img src={require('../img/finished-product.svg')} alt="Raw Materials Icon" className="cardIcon"/></Link>
+                    <label>Finished Product</label>
+                    <Link to="/need/product">
+                        <Button mode="strong" label="Next" className="actionButton"/>
+                    </Link>
+                </Card>
+            </div>
             </div>
         </div>
     );
